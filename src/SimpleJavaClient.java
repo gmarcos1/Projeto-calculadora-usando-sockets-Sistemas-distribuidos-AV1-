3973a1c0847f95b9053067e2151f8b6a91d7f0de
gmarcos1 <gugamarcos2017@gmail.com>
2022-10-28 21:26:21 -0300

    Calculadorafinalizada

---
+++ src/SimpleJavaClient.java
@@ -0,0 +1,29 @@
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Arrays;
+
+public class SimpleJavaClient{
+
+    public static void main(String[] args){
+        try {
+            Socket s = new Socket("127.0.0.1", 9999);
+            InputStream i = s.getInputStream();
+            OutputStream o = s.getOutputStream();
+            String str;
+            do {
+                byte[] line = new byte[100];
+                System.in.read(line);
+                o.write(line);
+                Arrays.fill(line, (byte) 0);
+                i.read(line);
+                str = new String(line);
+                System.out.println(str.trim());
+            } while ( !str.trim().equals("Sair") );
+            s.close();
+        }
+        catch (Exception err) {
+            System.err.println(err);}
+    }
+
+}
